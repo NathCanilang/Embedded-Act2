c36b204025fd42c995f1481aeac7242fec5870f1
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-09-05 17:18:28 +0800

    added chart and dummy data

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, Response, redirect, url_for, request, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/get_distance')
+def get_distance():
+    data = random.randint(2, 400)
+    if data:
+        return jsonify({
+            "distance" : data
+        }), 200
+    else:
+        return jsonify({
+            "Error" : "Error reading distance from sensor" 
+        }), 500
 
 if __name__ == '__main__':
     app.run()
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
   const timeText = document.getElementById("time-text");
+  const distanceText = document.getElementById("sensor-distance");
+
   function displayCurrentTime() {
     const currentTime = new Date();
     const formattedTime = currentTime.toLocaleTimeString();
     timeText.textContent = formattedTime;
   }
 
+  function displayDistance() {
+    fetch("/get_distance")
+      .then((response) => response.json())
+      .then((data) => {
+        distanceText.textContent = `Distance = ${data.distance} cm`;
+      })
+      .catch((error) => console.log(`Error fetching data: ${error}`));
+  }
+
+  const canvas = document.getElementById("ultrasonic-chart").getContext("2d");
+  const sensorScatterChart = new Chart(canvas, {
+    type: "scatter",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Distance (cm)",
+          data: [],
+          backgroundColor: "rgb(255, 99, 132)",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: "linear",
+          position: "bottom",
+        },
+      },
+    },
+  });
+
   setInterval(displayCurrentTime, 1000);
+  setInterval(displayDistance, 2000); //sinabi ito sa libro
 });
