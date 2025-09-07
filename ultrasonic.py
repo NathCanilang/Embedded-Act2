0bc7fdc2d55eb57828092358e05edc7671ff2edd
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-09-08 07:18:58 +0800

    changed u sonic baka umana

--- ultrasonic.py
+++ ultrasonic.py
@@ -15,35 +15,34 @@ class UltrasonicSensor:
         self.thread.start()
 
     def read_loop(self):
-    print("ultrasonic sensor has started")
-    while True:
-        # Send 10µs pulse
-        gpio.output(self.trigger_pin, True)
-        time.sleep(0.00001)
-        gpio.output(self.trigger_pin, False)
+        while True:
+            # Send 10µs pulse
+            gpio.output(self.trigger_pin, True)
+            time.sleep(0.00001)
+            gpio.output(self.trigger_pin, False)
 
-        # Wait for echo start (with timeout)
-        timeout = time.time() + 0.05  # 50ms
-        while gpio.input(self.echo_pin) == 0 and time.time() < timeout:
-            start_time = time.time()
+            # Wait for echo start (with timeout)
+            timeout = time.time() + 0.05  # 50ms
+            while gpio.input(self.echo_pin) == 0 and time.time() < timeout:
+                start_time = time.time()
 
-        if time.time() >= timeout:
-            self.curr_distance = None
-            time.sleep(0.1)
-            continue
+            if time.time() >= timeout:
+                self.curr_distance = None
+                time.sleep(0.1)
+                continue
 
-        # Wait for echo end (with timeout)
-        timeout = time.time() + 0.05
-        while gpio.input(self.echo_pin) == 1 and time.time() < timeout:
-            stop_time = time.time()
+            # Wait for echo end (with timeout)
+            timeout = time.time() + 0.05
+            while gpio.input(self.echo_pin) == 1 and time.time() < timeout:
+                stop_time = time.time()
 
-        if time.time() >= timeout:
-            self.curr_distance = None
-            time.sleep(0.1)
-            continue
+            if time.time() >= timeout:
+                self.curr_distance = None
+                time.sleep(0.1)
+                continue
 
-        # Calculate distance
-        time_elapsed = stop_time - start_time
-        self.curr_distance = (time_elapsed * 34300) / 2
+            # Calculate distance
+            time_elapsed = stop_time - start_time
+            self.curr_distance = (time_elapsed * 34300) / 2
 
-        time.sleep(0.1)  # avoid flooding
+            time.sleep(0.1)  # avoid flooding
