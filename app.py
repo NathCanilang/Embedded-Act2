4aa07115bb7e3c512e03ae7524e7a97a1b7bd1ab
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-09-07 20:17:23 +0800

    added the logic for the sensors, oled, and buzzer

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask, render_template, request, jsonify
 from database import SensorDatabase 
 import random
 
+#comment this imports to run the website without sensors
+from ultrasonic import UltrasonicSensor
+from oled import OLEDDisplay
+from buzzer import BuzzerController
+
 app = Flask(__name__)
 db = SensorDatabase()
 db.createTable()
+
+#GPIO pin settings ito
+s1_trig_pin = 17
+s1_echo_pin = 27
+
+s2_trig_pin = 5
+s2_echo_pin = 6
+#comment this code to run the website without sensors
+#also comment the display_sensor_values_oled code to run the site without sensors
+u_sensor1 = UltrasonicSensor(s1_trig_pin, s1_echo_pin)
+u_sensor2 = UltrasonicSensor(s2_trig_pin, s2_echo_pin)
+oled = OLEDDisplay()
+buzzer = BuzzerController()
 
 @app.route('/')
 def index():
@@ -45,7 +63,28 @@ def get_data_from_db():
         return jsonify(data)
     else:
         return jsonify({"error": "Data Error"}), 405
+    
+@app.route('/display_sensor_values_oled', methods = ["POST"])
+def display_sensor_values_oled():
+    data = request.get_json()
 
+    if not data:
+        return jsonify({"Error": "No values received" }), 400
+    
+    s1_distance = data.get("distance1")
+    s2_distance = data.get("distance2")
+    oled.display_distance(s1_distance, s2_distance)
+    return jsonify({"Success":"Values Received"}), 201
+
+@app.route('/start_buzzer', methods=["POST"])
+def start_buzzer():
+    buzzer.start()
+    return jsonify({"status": "buzzer started"})
+
+@app.route('/stop_buzzer', methods=["POST"])
+def stop_buzzer():
+    buzzer.stop()
+    return jsonify({"status": "buzzer stopped"})
 
 if __name__ == '__main__':
     with app.app_context():
---
+++ ultrasonic.py
@@ -0,0 +1,39 @@
+import time
+import threading
+import RPi.GPIO as gpio
+
+class UltrasonicSensor:
+    def __init__(self, trigger_pin, echo_pin):
+        self.trigger_pin = trigger_pin
+        self.echo_pin = echo_pin
+        self.curr_distance = 0
+        
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.trigger_pin, gpio.OUT)
+        gpio.setup(self.echo_pin, gpio.IN)
+        self.thread = threading.Thread(target=self.read_loop, daemon=True)    
+        self.thread.start()
+
+    def read_loop(self):
+        print("read has started")
+
+        while True:
+            gpio.output(self.trigger_pin, True)
+
+            time.sleep(0.1)
+            gpio.output(self.trigger_pin, False)
+
+            start_time = time.time()
+            stop_time = time.time()
+
+            while gpio.input(self.echo_pin) == 0:
+                start_time = time.time()
+
+            while gpio.input(self.echo_pin) == 1:
+                stop_time = time.time()
+
+            time_elapsed = stop_time - start_time 
+            self.curr_distance = (time_elapsed *34300) / 2
+
+    def get_current_distance(self):
+        return self.curr_distance
